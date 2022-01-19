bc3f6b403bd1a3f480de3d9ebd375f23db06aaed
m.baushenko <m.baushenko@artistrazh.tech>
2022-01-19 14:35:38 +0300

    reformatted for black style code

--- model_files/model.py
+++ model_files/model.py
@@ -2,10 +2,12 @@ from torchvision import models
 import torch
 
 
-class Model():
+class Model:
     def __init__(self, path):
         self.model = models.mobilenet_v2(pretrained=False)
-        self.model.classifier[1] = torch.nn.Linear(in_features=1280, out_features=2, bias=True)
+        self.model.classifier[1] = torch.nn.Linear(
+            in_features=1280, out_features=2, bias=True
+        )
         self.model.load_state_dict(torch.load(path, map_location=torch.device("cpu")))
         self.model.eval()
 
